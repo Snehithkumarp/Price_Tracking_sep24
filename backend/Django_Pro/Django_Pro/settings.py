c7be87f2d4dc5a3132c2cd45fdea4cc5671d3ba1
Snehithkumarp <125585610+Snehithkumarp@users.noreply.github.com>
2025-09-28 11:41:44 +0530

    Update settings.py

--- backend/Django_Pro/Django_Pro/settings.py
+++ backend/Django_Pro/Django_Pro/settings.py
@@ -179,8 +179,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "arya.pspk@gmail.com"          # Replace with your Gmail
-EMAIL_HOST_PASSWORD = "szxo czev vkwd mhfy"     # App password
+EMAIL_HOST_USER = ""          # Replace with your Gmail
+EMAIL_HOST_PASSWORD = ""     # App password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # -------------------------------
